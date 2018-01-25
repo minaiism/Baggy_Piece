596425b700092386a0e35ceefe71c8f1a6479473
Martyna Wójcik <martyna.wojcik.mail@gmail.com>
2018-01-25 01:41:57 +0100

    layout

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.post("/register", function(req, res) {
     if (err) {
       console.log(err);
       req.flash("error", err.message);
-      return res.render("register");
+      res.redirect("/register");
     } else {
       passport.authenticate("local")(req, res, function() {
         req.flash("success", "Welcome to BaggyPiece" + " " + user.username);
